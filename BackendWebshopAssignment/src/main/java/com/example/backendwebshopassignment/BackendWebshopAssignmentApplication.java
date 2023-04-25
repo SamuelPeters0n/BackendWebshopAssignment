4babe945933f0a6da799ddf580cadedfae76c286
SamuelPeters0n <114670965+SamuelPeters0n@users.noreply.github.com>
2023-04-25 15:06:45 +0200

    Initial Commit

---
+++ BackendWebshopAssignment/src/main/java/com/example/backendwebshopassignment/BackendWebshopAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendwebshopassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendWebshopAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendWebshopAssignmentApplication.class, args);
+    }
+
+}
