7dbff165966fac2ff9e3ae40d1f1eacf025c27d0
SamuelPeters0n <sampet02@gmail.com>
2023-04-27 12:42:24 +0200

    Added instructions as comments

--- BackendWebshopAssignment/src/main/java/com/example/backendwebshopassignment/BackendWebshopAssignmentApplication.java
+++ BackendWebshopAssignment/src/main/java/com/example/backendwebshopassignment/BackendWebshopAssignmentApplication.java
@@ -6,6 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BackendWebshopAssignmentApplication {
 
+    //Database and user needs to be created in SQL, use following code:
+    //-------------------------------------------------------------
+    // create database BackendWebshopDatabase;
+    // create user 'springuser@localhost' identified by '12345678';
+    // grant all on *.* to 'springuser@localhost';
+    //-------------------------------------------------------------
     public static void main(String[] args) {
         SpringApplication.run(BackendWebshopAssignmentApplication.class, args);
     }
